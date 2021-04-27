1206c8f7c0603b2c8482493ab5a39aa5b18d6303
Juha Ala-Rantala <alarantala.juha@gmail.com>
2021-04-27 16:53:28 +0300

    wip

--- GameHighlightClipper/App.xaml.cs
+++ GameHighlightClipper/App.xaml.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using GameHighlightClipper.MVVM.Models.Interfaces;
+using GameHighlightClipper.MVVM.Models.Services;
+using System;
 using System.Windows;
+using Unity;
 
 namespace GameHighlightClipper
 {
@@ -13,5 +11,39 @@ namespace GameHighlightClipper
     /// </summary>
     public partial class App : Application
     {
+        INLogLogger logger;
+
+        public App()
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(ExceptionHandler);
+
+            IUnityContainer container = new UnityContainer();
+            container.RegisterType<IVideoProcessingService, VideoProcessingService>();
+            container.RegisterType<INLogLogger, NLogLogger>();
+
+            logger = container.Resolve<INLogLogger>();
+
+            try
+            {
+                throw new Exception("1");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Catch clause caught : {0} \n", e.Message);
+            }
+
+            throw new Exception("2");
+        }
+
+        void ExceptionHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            logger.Trace(e);
+            if (e.InnerException != null)
+            {
+                logger.Trace(e.InnerException);
+            }
+        }
     }
 }
--- GameHighlightClipper/MVVM/Models/Interfaces/IVideoProcessingService.cs
+++ GameHighlightClipper/MVVM/Models/Interfaces/IVideoProcessingService.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using GameHighlightClipper.Helpers;
 
 namespace GameHighlightClipper.MVVM.Models.Interfaces
 {
